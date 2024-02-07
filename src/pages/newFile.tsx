4ba54818a6d3937566e3794ec803cface31fdb8f
Jonathan <121680615+juanfercito@users.noreply.github.com>
2024-02-06 23:11:48 -0500

    creating new project as portfolio

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,24 @@
+import Layout from "../layouts/Layout.astro";
+import Badge from "../components/Badge.astro";
+
+<Fragment>
+<Layout title="Welcome to Astro.">
+<main>
+<section class="w-full mx-auto lg:w-[740px] py-44">
+<h1 class="text-white text-5xl font-bold flex flex-row gap-x-4 pb-4">{`
+        Hey, I'm Juanfercito
+        `}<a href="https://www.linkedin.com/in/jonathan-lozano-moreira-11b6b3263/" target="-blank" rel="noopener" class="flex justify-center items-center">
+<Badge>Available for Hire</Badge>
+</a>
+</h1>
+<h2 class="text-2xl opacity-80 text-wrap">
++1 años de experiencia. Desarrollador Web. De manta, Manabí, Ecuador 🇪🇨.
+\u{1}F1EA}\u{1}F1E8} Creación de aplicaciones Eficientes y atractivas
+</h2>
+
+<ul></ul>
+</section>
+</main>
+</Layout>
+
+</Fragment>;
